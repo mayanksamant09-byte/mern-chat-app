f2c65cc6f76cc723585d6f25947b0bc60797da51
Mayank Samant <mayanksamant09@gmail.com>
2026-05-04 03:45:43 +0530

    Hardcoded MongoDB connection and updated project files

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
+		await mongoose.connect("mongodb+srv://mayanksamant09_db_user:zDusgsHjQ83BXaKR@chatapp.thf346v.mongodb.net/chat-app?retryWrites=true&w=majority&appName=chatapp");
+		console.log("MongoDB Connected");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
 	}
