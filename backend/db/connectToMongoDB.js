bcd9a0e72305ef87d937efc723198a778af67e6e
Mayank Samant <mayanksamant09@gmail.com>
2026-05-04 03:52:25 +0530

    Hardcoded MongoDD connection...

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
+	console.log("URI used:", "mongodb+srv://mayanksamant09_db_user..."); // ye line add kar
 	try {
 		await mongoose.connect("mongodb+srv://mayanksamant09_db_user:zDusgsHjQ83BXaKR@chatapp.thf346v.mongodb.net/chat-app?retryWrites=true&w=majority&appName=chatapp");
 		console.log("MongoDB Connected");
